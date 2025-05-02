5189102d07a126e4537532ea1257f673eff7f52f
Étienne Doré <119957024+etiennedo@users.noreply.github.com>
2025-05-01 22:58:45 -0400

    initial theme changes

--- src/consts.ts
+++ src/consts.ts
@@ -1,48 +1,36 @@
 import type { IconMap, SocialLink, Site } from '@/types'
 
 export const SITE: Site = {
-  title: 'astro-erudite',
+  title: 'étienne',
   description:
-    'astro-erudite is a opinionated, unstyled blogging template—built with Astro, Tailwind, and shadcn/ui.',
-  href: 'https://astro-erudite.vercel.app',
-  author: 'jktrn',
-  locale: 'en-US',
+    'Étienne Doré est un dev front-end qui a créé son blog avec le thème astro-erudite et Github Pages.',
+  href: 'https://https://etiennedo.github.io/astro-blog',
+  author: 'etiennedo',
+  locale: 'fr-CA',
   featuredPostCount: 2,
   postsPerPage: 3,
 }
 
 export const NAV_LINKS: SocialLink[] = [
   {
-    href: '/blog',
-    label: 'blog',
-  },
-  {
-    href: '/authors',
-    label: 'authors',
-  },
-  {
-    href: '/about',
-    label: 'about',
-  },
+    href: '/astro-blog/blog',
+    label: 'articles',
+  }
 ]
 
 export const SOCIAL_LINKS: SocialLink[] = [
   {
-    href: 'https://github.com/jktrn',
+    href: 'https://github.com/etiennedo',
     label: 'GitHub',
   },
   {
-    href: 'https://twitter.com/enscry',
+    href: 'https://twitter.com/etiennedore99',
     label: 'Twitter',
   },
   {
-    href: 'mailto:jason@enscribe.dev',
+    href: 'mailto:etiennexdore@gmail.com',
     label: 'Email',
-  },
-  {
-    href: '/rss.xml',
-    label: 'RSS',
-  },
+  }
 ]
 
 export const ICON_MAP: IconMap = {
