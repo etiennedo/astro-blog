bf3bd201027cd58631e0d8aeb2847ddcd580b6c5
Étienne Doré <119957024+etiennedo@users.noreply.github.com>
2025-05-01 21:42:51 -0400

    config gh pages

--- astro.config.ts
+++ astro.config.ts
@@ -21,7 +21,8 @@ import { pluginLineNumbers } from '@expressive-code/plugin-line-numbers'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  site: 'https://astro-erudite.vercel.app',
+  site: 'https://etiennedo.github.io',
+  base: '/astro-blog',
   integrations: [
     expressiveCode({
       themes: ['github-light', 'github-dark'],
